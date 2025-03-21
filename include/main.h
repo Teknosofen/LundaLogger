136f3b5077cf209f2e4c6dab7c9e1a0858a08df5
Åke Larsson <ake.larsson@getinge.com>
2025-03-21 14:42:43 +0100

    addisng some variant files, to be pruged later

---
+++ include/main.h
@@ -0,0 +1,24 @@
+
+#ifndef MAIN_h
+#define MAIN_h
+
+#include <Arduino.h>
+
+
+// -----------------------------------------------
+//
+// main.hpp for the Lundalogger
+//
+// Åke L 2025-03
+//
+// -----------------------------------------------
+
+#define lundaLoggerVerLbl "LundaLogger 2025-03-20 1.0.0"
+
+#define hostCom Serial
+#define servoCom Serial2
+
+#include "ServoCIEData.hpp"
+
+
+#endif
